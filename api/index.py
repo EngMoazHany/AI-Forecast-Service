e0c65311becd4d955e3c0d704bfe14b7e3daa529
Moaz Hany <youremail@example.com>
2026-02-24 03:41:36 +0200

    enable asgi handler

--- api/index.py
+++ api/index.py
@@ -10,3 +10,8 @@ def health():
 @app.post("/api/forecast")
 async def forecast(dto: dict):
     return run_forecast(dto["series"], dto.get("forecast_horizon", 1))
+
+
+# 👇 مهم جدًا للسيرفرلس
+def handler(request):
+    return app(request.scope, request.receive, request.send)
